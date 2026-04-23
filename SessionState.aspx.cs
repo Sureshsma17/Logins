f3436f50015aff9f8bdb5e72eda5f115523e7cde
Suresh Miriyala <sureshmiriyala17@gmail.com>
2026-04-23 11:08:47 +0530

    sessionstate

---
+++ SessionState.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Logins
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["ClickCount"]==null)
+            {
+                Session["ClickCount"] = 0;
+            }
+            lblCount.Text= "Click Count " + Session["ClickCount"].ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int Count = (int)Session["ClickCount"];
+            Count++;
+            Session["ClickCount"] = Count;
+            lblCount.Text = "Click Count " + Session["ClickCount"].ToString();
+
+        }
+    }
+}
